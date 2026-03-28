b320fc460eaaf28cfe4bb1048e093e2e8c3ac83b
rhantschk-cmyk <rhantschk@gmail.com>
2026-03-28 11:27:42 +0100

    Gravity
    The player is now falling towards the ground if touching_ground is false

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,14 @@
 const float WIDTH = 1000.f;
 const float HEIGHT = 1600.f;
 
+
+
 class Player {
 public:
     sf::RectangleShape shape;
+    bool touching_ground = false;
+    const float gravity_force = 0.05f;
+    float falling_speed= 0.f;
 
     Player() {
         shape.setSize(sf::Vector2f(20.f, 20.f));
@@ -17,8 +22,35 @@ public:
         shape.setPosition(50.f, 50.f);
     }
 
+    void gravity() {
+        if (touching_ground == false) {
+            falling_speed += gravity_force;
+        } else {
+            falling_speed = 0.f;
+        }
+        shape.move(0.f, falling_speed);
+    }
+
     void draw(sf::RenderWindow& window) {window.draw(shape);}
 };
+
+//Functions
+
+void update_window(sf::RenderWindow& window) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            window.close();
+        }
+    window.display();
+    window.clear(sf::Color::Black);
+}
+
+void update_player(sf::RenderWindow& window, Player& player) {
+    player.draw(window);
+    player.gravity();
+}
 
 //Mainloop
 
@@ -27,15 +59,8 @@ int main() {
     Player player;
 
     while (window.isOpen()) {
-        sf::Event event;
-
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        player.draw(window);
-        window.display();
-        window.clear(sf::Color::Black);
+        update_player(window, player);
+        update_window(window);
     }
     return 0;
 }
