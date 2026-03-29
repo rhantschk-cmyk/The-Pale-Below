ed2201ad5a6e00f672fb4165181bcdfb4d5bff21
rhantschk-cmyk <rhantschk@gmail.com>
2026-03-29 10:13:48 +0200

    Comments
    I added Comments for understanding Code. no changes in logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,22 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
+//Window
 const float WIDTH = 1000.f;
 const float HEIGHT = 1600.f;
 
 
+//Classes
 
 class Player {
 public:
+    //Player Hitbox
     sf::RectangleShape shape;
+    //If true Player will fall downwards
     bool touching_ground = false;
+    //How fast the Player gains speed when falling
     const float gravity_force = 0.05f;
+    //actual downspeed of Player
     float falling_speed= 0.f;
 
     Player() {
@@ -22,7 +28,7 @@ public:
         shape.setFillColor(sf::Color::Blue);
         shape.setPosition(50.f, 50.f);
     }
-
+    //Falling if not touching ground
     void gravity() {
         if (touching_ground == false) {
             falling_speed += gravity_force;
@@ -31,7 +37,7 @@ public:
         }
         shape.move(0.f, falling_speed);
     }
-
+    //This function should be called when player is touching ground
     void collides(int i) {
         if (i == 1) {
             touching_ground = true;
@@ -39,43 +45,48 @@ public:
             touching_ground = false;
         }
     }
-
+    //Draw Function für window logic
     void draw(sf::RenderWindow& window) {window.draw(shape);}
 };
 
 class Platform {
 public:
-    sf::RectangleShape shape;
+    sf::RectangleShape shape; //Platform Hitbox
 
     Platform(const float x, const float y, const float width, const float height) {
         shape.setSize(sf::Vector2f(width, height));
         shape.setFillColor(sf::Color::Red);
         shape.setPosition(x, y);
     }
-
+    //Draw function for window logic
     void draw(sf::RenderWindow& window) {window.draw(shape);}
 };
 
 //Functions
 
-bool collision(const sf::RectangleShape& a, const sf::RectangleShape& b) {return a.getGlobalBounds().intersects(b.getGlobalBounds());}
+bool collision(const sf::RectangleShape& a, const sf::RectangleShape& b) {return a.getGlobalBounds().intersects(b.getGlobalBounds());} //notices collision between 2 sf::RectangleShapes (Hitboxes)
 
 void update_window(sf::RenderWindow& window) {
-    sf::Event event;
-
+    sf::Event event; //For window poll logic
+    //Closing window when Closing Button is pressed
     while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed)
             window.close();
         }
+    //Display everything
     window.display();
     window.clear(sf::Color::Black);
 }
 
+//Player function
+//All Player Code Here
 void update_player(sf::RenderWindow& window, Player& player) {
     player.draw(window);
     player.gravity();
 }
 
+//Platform function
+//includes Player collision with platforms
 void update_platforms(std::vector<Platform>& platforms, sf::RenderWindow& window, Player& player) {
     for (auto& platform : platforms) {
         if (collision(platform.shape, player.shape)) {
@@ -88,16 +99,20 @@ void update_platforms(std::vector<Platform>& platforms, sf::RenderWindow& window
 //Mainloop
 
 int main() {
+    //Initialization 
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "2D Game");
     Player player;
     std::vector<Platform> platforms;
 
+    //Test-Platform
     platforms.push_back(Platform(30.f, 70.f, 100.f, 10.f));
 
+    //Calling update functions while window not closed
     while (window.isOpen()) {
         update_player(window, player);
         update_platforms(platforms, window, player);
         update_window(window);
     }
+    //End of the Code
     return 0;
 }
